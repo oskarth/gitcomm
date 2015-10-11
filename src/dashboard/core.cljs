34ffde289207586d14ccf2af360bad62cc48da40
oskarth <me@oskarth.com>
2015-10-11 15:27:32 +0200

    Cleanup core app

--- src/dashboard/core.cljs
+++ src/dashboard/core.cljs
@@ -4,16 +4,19 @@
            [reagent.core :as r :refer [atom]]
            [cljsjs.firebase]))
 
+;; Config
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;(enable-console-print!) ; print to terminal
 (fc/enable-repl-print!) ; print to repl
 
-;; Application state
-(defonce oskarth (r/atom {}))
+(def fb-ref (js/Firebase. "https://intense-heat-8207.firebaseio.com"))
+
+;; State
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defonce gh-user (r/atom {}))
-(defonce gh-koan (r/atom 0))
 (defonce app-state (atom {:text "Hello Oskar!"}))
 
-(def fb-ref (js/Firebase. "https://intense-heat-8207.firebaseio.com"))
 
 (defn auth-fb! []
  (.authWithOAuthPopup fb-ref "github"
@@ -22,35 +25,6 @@
          (println "Login failed" error)
          (swap! gh-user (fn [_] (js->clj data)))))))
 
-; GET https://github.com/login/oauth/authorize
-; how to send in params?
-
-(defn handler [response]
-  (println (str response)))
-
-(defn error-handler [{:keys [status status-text]}]
-  (println (str "something bad happened: " status " " status-text)))
-
-(defn get-oskarth! []
-  (GET "https://api.github.com/users/oskarth"
-       {:handler handler
-        :error-handler error-handler}))
-
-;; Seems like the best way to do an in-browser redirect. Maybe just replace.
-(defn set-location! [url]
-  (set! (.-location js/window) url))
-
-; this should happen first req no?
-(defn get-code-from-url []
-  (let [params (str (.-search (.-location js/window)))]
-    (second (re-matches #"\?code=(.*)" params))))
-
-;; When is this done so we can get code from url?
-;; Also why is this so hacky :< Some wait for code to appear? Bah.
-(defn auth-github! []
-    (set-location!
-    "https://github.com/login/oauth/authorize?client_id=1cdf28ef2c9c5df3827f&redirect_uri=http://localhost:3449&callback=http://localhost:3449"))
-
 (defn req!
   "Dispatches a request on key."
   [key]
@@ -58,22 +32,7 @@
         :else (println "Error, no such key:" key)))
 
 ;; Components
-
-; Not a good solution. Instead, when we click button we put it on a queue or
-; whatever. Event loop thingy like you had with your om thing. Use that!
-(defn counting-component []
-  [:div
-    "The atom " [:code "gh-koan"] " has value: "
-    @gh-koan ". "
-    [:input {:type "button" :value "Click me to get it!"
-             :on-click #(swap! gh-koan inc)}]])
-
-(defn timer-component []
-  (let [seconds-elapsed (r/atom 0)]
-    (fn []
-      (js/setTimeout #(swap! seconds-elapsed inc) 1000)
-      [:div
-        "Seconds Elapsed: " @seconds-elapsed])))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn global-nav []
   [:nav.clearfix
@@ -82,27 +41,14 @@
     [:div.col-right
       [:a.btn.py2 {:href "/"} "About"]]])
 
-(defn authed-component []
- [:div
-  [:p.h3 (str "Welcome " (get-code-from-url) "!")]
-  [:a.h3.btn.btn-primary.mb4.black.bg-yellow
-   {:on-click #(req! :foo!)} "Find someone to play with"]])
-
-(defn unauthed-component []
- [:div
-   [:p.h3 "A game of commitment"]
-   [:a.h3.btn.btn-primary.mb4.black.bg-yellow
-   {:on-click #(req! :auth-github!)} "Play with Github"]])
-
 (defn main-component []
   [:header
     (global-nav)
     [:div.center.px2.py4
       [:h2.h2-responsive.caps.mt4.mb0.regular "gitcomm"]
-      ;; TODO: Check auth-state too, and how know if real?
-      (if (get-code-from-url)
-          (authed-component)
-          (unauthed-component))]])
+      [:p.h3 "A game of commitment"]
+      [:a.h3.btn.btn-primary.mb4.black.bg-yellow
+        {:on-click #(req! :auth-github!)} "Play with Github"]]])
 
 (defn hello-world []
   [:h1 (:text @app-state)])
